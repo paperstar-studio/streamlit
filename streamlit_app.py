e6581ce03395f56e60afbb43c4966134a2590015
paperstar-studio <166758222+paperstar-studio@users.noreply.github.com>
2026-04-11 20:51:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,12 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+pages = {
+    "running": [
+        st.Page("running.py", title="running data"),
+    ],
+}
+
+pg = st.navigation(pages, position="top")
